f07ee173f467daca68be896fb34139ea1a54b3ba
Alexander Bouthillet <alex.bouthillet@gmail.com>
2025-02-07 12:28:02 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+
+  return (
+    <>
+      <div id="container">
+        <h1>Hello React Router!</h1>
+        <div id="navbar">{/* navigation here */}</div>
+        <div id="main-section">{/* routes here */}</div>
+      </div>
+    </>
+  )
+}
+
+export default App
